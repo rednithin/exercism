1dfb5ebf4cf203a40ae37760bfbffd80ff58dc6e
rednithin <reddy.nithinpg@live.com>
2019-08-18 00:59:52 +0530

    Made the number readable.

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,7 @@
-use chrono::{Duration, DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(10_i64.pow(9))
+    // start + Duration::seconds(10_i64.pow(9))
+    start + Duration::seconds(1_000_000_000)
 }
