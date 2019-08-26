a5ceb1e375188bc5d8aaf4b97659356d1bf8e521
rednithin <reddy.nithinpg@live.com>
2019-08-26 08:26:56 +0530

    Made changes to nth prime.

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,32 +1,25 @@
 pub fn nth(n: u32) -> u32 {
-    let mut primes = vec![2];
-    let mut num = 3;
+    let mut primes: Vec<u32> = vec![2];
     let mut len = 1;
 
-    loop {
-        let mut is_prime = true;
-        for prime in primes.iter() {
-            if num % *prime == 0 {
-                is_prime = false;
-                break;
-            }
-            if *prime as f64 > (num as f64).sqrt() {
-                break;
-            }
-        }
-        if is_prime {
+    let is_prime = |num, primes: &Vec<u32>| {
+        !primes
+            .iter()
+            .filter(|&prime| *prime as f64 <= (num as f64).sqrt())
+            .map(|&prime| num % prime == 0)
+            .any(|x| x == true)
+    };
+    for num in (3..).step_by(2) {
+        if is_prime(num, &primes) {
             primes.push(num);
             len += 1;
         }
-        num += 1;
         // println!("{:?}", primes);
         if len >= n + 1 {
-            match primes.get(n as usize) {
-                Some(the_prime) => {
-                    return the_prime.clone();
-                }
-                None => panic!("Lol"),
-            };
+            if let Some(the_prime) = primes.get(n as usize) {
+                return the_prime.clone();
+            }
         }
     }
+    unreachable!();
 }
--- rust/nth-prime/src/main.rs
+++ rust/nth-prime/src/main.rs
@@ -1,5 +1,11 @@
 mod lib;
 
 fn main() {
+    println!(
+        "{:?}",
+        (0..15)
+            .map(|x: i32| lib::nth(x as u32))
+            .collect::<Vec<u32>>()
+    );
     println!("{}", lib::nth(10000));
 }
