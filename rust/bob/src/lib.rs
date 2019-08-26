f98f2b365ba7ce5c1b384f836aaad75bfaf73ee5
rednithin <reddy.nithinpg@live.com>
2019-08-27 00:12:02 +0530

    Finished changes to bob

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -8,11 +8,14 @@ fn is_yelling(message: &str) -> bool {
 
 pub fn reply(message: &str) -> &str {
     let t = message.trim();
-    match (t.len(), t.ends_with('?'), is_yelling(t)) {
-        (0, _, _) => "Fine. Be that way!",
-        (_, true, true) => "Calm down, I know what I'm doing!",
-        (_, true, _) => "Sure.",
-        (_, _, true) => "Whoa, chill out!",
-        _ => "Whatever.",
+    if t.is_empty() {
+        "Fine. Be that way!"
+    } else {
+        match (t.ends_with('?'), is_yelling(t)) {
+            (true, true) => "Calm down, I know what I'm doing!",
+            (true, _) => "Sure.",
+            (_, true) => "Whoa, chill out!",
+            _ => "Whatever.",
+        }
     }
 }
