06a42f1f121ecdadc20cb2ef31ad9eb8b7f99fa1
rednithin <reddy.nithinpg@live.com>
2019-08-28 09:56:19 +0530

    Optimized Prime factors

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,15 +1,17 @@
 pub fn factors(n: u64) -> Vec<u64> {
     let mut num = n;
-    let mut prime_numbers = vec![2];
+    let mut prime_numbers = vec![2 as u64];
     let mut number_factors = Vec::new();
-    let mut divisor = 3;
+    let mut divisor: u64 = 3;
     while num % 2 == 0 {
         num /= 2;
-        number_factors.push(2);
+        number_factors.push(2 as u64);
     }
     while num != 1 {
+        let sqrt_divisor = (divisor as f64).sqrt() as u64;
         if !prime_numbers
             .iter()
+            .take_while(|&prime_number| *prime_number <= sqrt_divisor)
             .any(|prime_number| divisor % prime_number == 0)
         {
             prime_numbers.push(divisor);
