b3ddacec200890a098d0ebb29e9f7cbfd3d33133
rednithin <reddy.nithinpg@live.com>
2019-09-22 20:11:42 +0530

    Done with changes suggested to Atbash Cipher.

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,37 +1,51 @@
 /// "Encipher" with the Atbash cipher.
+
+const ASCII_SMALL_A: u8 = 97;
+const ASCII_SMALL_Z: u8 = 122;
+const ASCII_SUM: u8 = ASCII_SMALL_A + ASCII_SMALL_Z;
+
 pub fn encode(plain: &str) -> String {
     let mut len = 0;
-    plain.to_lowercase()
+    plain
+        .to_lowercase()
         .chars()
-        .filter(|c| c.is_ascii_alphanumeric())
-        .map(|c| {
-            if c.is_numeric() { 
-                c
-            } else {
-                (219 - c as u8) as char
+        .filter_map(|c| {
+            if c.is_ascii_alphanumeric() {
+                if c.is_numeric() {
+                    return Some(c);
+                } else {
+                    return Some((ASCII_SUM - c as u8) as char);
+                }
             }
-        } ).fold(String::new(), |mut i, j| {
-            if len == 5 {
-                i.push(' ');
-                len = 0;
-            }
-            len += 1;
-            i.push(j); 
-            i
+            None
         })
+        .fold(
+            String::with_capacity(plain.len() as usize + plain.len() / 5 as usize),
+            |mut i, j| {
+                if len == 5 {
+                    i.push(' ');
+                    len = 0;
+                }
+                len += 1;
+                i.push(j);
+                i
+            },
+        )
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
     cipher
         .chars()
-        .filter(|c| c.is_ascii_alphanumeric())
-        .map(|c| {
-            if c.is_numeric() { 
-                c
-            } else {
-                (219 - c as u8) as char
+        .filter_map(|c| {
+            if c.is_ascii_alphanumeric() {
+                if c.is_numeric() {
+                    return Some(c);
+                } else {
+                    return Some((ASCII_SUM - c as u8) as char);
+                }
             }
+            None
         })
         .collect()
 }
